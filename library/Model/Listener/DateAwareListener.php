0d41a4eb994f8f5e2fe96dd80f457cac37e84871
visa4 <antonino.visalli@gmail.com>
2016-01-08 20:17:05 +0100

    Replace date aware listener

---
+++ library/Model/Listener/DateAwareListener.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace namespace Strapieno\Utils\Model\Listener;
+
+use DateTime;
+use Matryoshka\Model\ModelEvent;
+use Strapieno\Utils\Model\Entity\DateHistoryAwareInterface;
+use Zend\EventManager\EventManagerInterface;
+use Zend\EventManager\ListenerAggregateInterface;
+use Zend\EventManager\ListenerAggregateTrait;
+
+/**
+ * Class DateAwareListener
+ */
+class DateAwareListener implements ListenerAggregateInterface
+{
+    use ListenerAggregateTrait;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attach(EventManagerInterface $events)
+    {
+        $this->listeners[] = $events->attach(ModelEvent::EVENT_SAVE_PRE, function (ModelEvent $event) {
+            $object = $event->getData();
+
+            if ($object instanceof DateHistoryAwareInterface) {
+                $now = new DateTime;
+                $object->setDateModified($now);
+                if (!$object->getDateCreated()) {
+                    $object->setDateCreated($now);
+                }
+            }
+        });
+    }
+}
