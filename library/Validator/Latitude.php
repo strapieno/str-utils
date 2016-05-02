50eb78a16631e30dca18157b6d112bd716061945
visa4 <antonino.visalli@gmail.com>
2016-05-02 13:13:00 +0200

    Add latitude and longitude validator

---
+++ library/Validator/Latitude.php
@@ -0,0 +1,31 @@
+<?php
+namespace Strapieno\Utils\Validator;
+
+use Zend\Validator\AbstractValidator;
+
+/**
+ * Class Longitude
+ */
+class Latitude extends AbstractValidator
+{
+    const INVALID_LATIDUDE = 'invalidLatitude';
+
+    private $regex = "/^-?([1-8]?[1-9]|[1-9]0)\.{1}\d{1,6}$/";
+
+    protected $messageTemplates = [
+        self::INVALID_LATIDUDE  => 'Latitude format not valid'
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isValid($value)
+    {
+        if (!preg_match($this->regex, $value)) {
+            $this->error(self::INVALID_LATIDUDE);
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ library/Validator/Longitude.php
@@ -0,0 +1,31 @@
+<?php
+namespace Strapieno\Utils\Validator;
+
+use Zend\Validator\AbstractValidator;
+
+/**
+ * Class Longitude
+ */
+class Longitude extends AbstractValidator
+{
+    const INVALID_LONGITUDE  = 'invalidLongitude';
+
+    private $regex = "/^-?([1]?[1-7][1-9]|[1]?[1-8][0]|[1-9]?[0-9])\.{1}\d{1,6}$/";
+
+    protected $messageTemplates = [
+        self::INVALID_LONGITUDE  => 'Longitude format not valid'
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isValid($value)
+    {
+        if (!preg_match($this->regex, $value)) {
+            $this->error(self::INVALID_LONGITUDE);
+            return false;
+        }
+
+        return true;
+    }
+}
