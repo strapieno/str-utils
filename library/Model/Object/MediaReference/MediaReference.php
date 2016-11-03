cfa1b3887ecc0ff28280a09e494d882827179a6b
visa4 <antonino.visalli@gmail.com>
2016-11-03 18:26:56 +0100

    Add media reference

--- library/Model/Object/MediaReference/MediaReference.php
+++ library/Model/Object/MediaReference/MediaReference.php
@@ -2,13 +2,26 @@
 namespace Strapieno\Utils\Model\Object\MediaReference;
 
 use Strapieno\Utils\Model\Object\AbstractObject;
+use Strapieno\Utils\Model\Object\EntityReference\EntityReferenceAwareInterface;
+use Strapieno\Utils\Model\Object\EntityReference\EntityReferenceAwareTrait;
 use Strapieno\Utils\Model\Object\Media\MediaInterface;
+use Strapieno\Utils\Model\Object\Media\MediaTrait;
 use Strapieno\Utils\Model\Object\RefIdentityInterface;
 
 /**
  * Class MediaReference
  */
-class MediaReference extends AbstractObject implements RefIdentityInterface, MediaInterface, ReferenceAwareInterfac
+class MediaReference extends AbstractObject implements RefIdentityInterface, MediaInterface, EntityReferenceAwareInterface
 {
+    use EntityReferenceAwareTrait;
+    use MediaTrait;
 
+    public function getRefIdentity()
+    {
+        if (!$this->entityReference) {
+            throw new \RuntimeException('entityReference must be set');
+        }
+
+        $this->getEntityReference()->getId();
+    }
 }
